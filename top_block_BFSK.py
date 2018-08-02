e0a5f974de8062255dda4ef279057b10745d0488
lucasebs <lucasebatista@gmail.com>
2018-08-02 13:08:14 -0300

    FSK renomeado para BFSK

---
+++ top_block_BFSK.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block Bfsk
+# Generated: Thu Aug  2 12:57:45 2018
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import constsink_gl
+from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import scopesink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import math
+import wx
+
+
+class top_block_BFSK(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block Bfsk")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 44100
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.notebook_0 = self.notebook_0 = wx.Notebook(self.GetWin(), style=wx.NB_TOP)
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "fft")
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "ScopeRx")
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "Constellation")
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "ScopeConstellation")
+        self.Add(self.notebook_0)
+        self.wxgui_scopesink2_0 = scopesink2.scope_sink_f(
+        	self.notebook_0.GetPage(1).GetWin(),
+        	title="Scope Plot - Source - Modulated - Demodulated",
+        	sample_rate=samp_rate,
+        	v_scale=1,
+        	v_offset=0,
+        	t_scale=0,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=3,
+        	trig_mode=wxgui.TRIG_MODE_AUTO,
+        	y_axis_label="Counts",
+        )
+        self.notebook_0.GetPage(1).Add(self.wxgui_scopesink2_0.win)
+        self.wxgui_fftsink2_0_0 = fftsink2.fft_sink_f(
+        	self.notebook_0.GetPage(0).GetWin(),
+        	baseband_freq=0,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot Source",
+        	peak_hold=False,
+        )
+        self.notebook_0.GetPage(0).Add(self.wxgui_fftsink2_0_0.win)
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_f(
+        	self.notebook_0.GetPage(0).GetWin(),
+        	baseband_freq=0,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=30,
+        	ref_scale=4,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot Demodulated",
+        	peak_hold=False,
+        )
+        self.notebook_0.GetPage(0).Add(self.wxgui_fftsink2_0.win)
+        self.wxgui_constellationsink2_0_0 = constsink_gl.const_sink_c(
+        	self.notebook_0.GetPage(2).GetWin(),
+        	title="Constellation Plot Modulated",
+        	sample_rate=samp_rate,
+        	frame_rate=5,
+        	const_size=2048,
+        	M=4,
+        	theta=0,
+        	loop_bw=6.28/100.0,
+        	fmax=0.06,
+        	mu=0.5,
+        	gain_mu=0.005,
+        	symbol_rate=samp_rate/4.,
+        	omega_limit=0.005,
+        )
+        self.notebook_0.GetPage(2).Add(self.wxgui_constellationsink2_0_0.win)
+        self.wxgui_constellationsink2_0 = constsink_gl.const_sink_c(
+        	self.notebook_0.GetPage(3).GetWin(),
+        	title="Constellation Plot Demodulated",
+        	sample_rate=samp_rate,
+        	frame_rate=5,
+        	const_size=2048,
+        	M=4,
+        	theta=0,
+        	loop_bw=6.28/100.0,
+        	fmax=0.06,
+        	mu=0.5,
+        	gain_mu=0.005,
+        	symbol_rate=samp_rate/4.,
+        	omega_limit=0.005,
+        )
+        self.notebook_0.GetPage(3).Add(self.wxgui_constellationsink2_0.win)
+        self.nb1 = self.nb1 = wx.Notebook(self.GetWin(), style=wx.NB_LEFT)
+        self.nb1.AddPage(grc_wxgui.Panel(self.nb1), "fftsource")
+        self.Add(self.nb1)
+        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, samp_rate, 8000, 1000, firdes.WIN_HAMMING, 6.76))
+        self.digital_binary_slicer_fb_0 = digital.binary_slicer_fb()
+        self.blocks_vector_source_x_0_1 = blocks.vector_source_b((1,1,0,1), True, 1, [])
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.blocks_sub_xx_1 = blocks.sub_ff(1)
+        self.blocks_sub_xx_0 = blocks.sub_cc(1)
+        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_char*1, 100)
+        self.blocks_multiply_xx_0_0 = blocks.multiply_vcc(1)
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.blocks_float_to_complex_1 = blocks.float_to_complex(1)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
+        self.blocks_char_to_float_1 = blocks.char_to_float(1, 1)
+        self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
+        self.blocks_add_xx_0 = blocks.add_vcc(1)
+        self.analog_sig_source_x_0_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 400, 5, 0)
+        self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 44000, 5, 0)
+        self.analog_quadrature_demod_cf_0 = analog.quadrature_demod_cf(-100)
+        self.analog_const_source_x_0_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 3)
+        self.analog_const_source_x_0 = analog.sig_source_c(0, analog.GR_CONST_WAVE, 0, 0, 1)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_const_source_x_0, 0), (self.blocks_sub_xx_0, 0))    
+        self.connect((self.analog_const_source_x_0_0, 0), (self.blocks_sub_xx_1, 1))    
+        self.connect((self.analog_quadrature_demod_cf_0, 0), (self.digital_binary_slicer_fb_0, 0))    
+        self.connect((self.analog_quadrature_demod_cf_0, 0), (self.wxgui_fftsink2_0, 0))    
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0, 0))    
+        self.connect((self.analog_sig_source_x_0_0_0, 0), (self.blocks_multiply_xx_0_0, 0))    
+        self.connect((self.blocks_add_xx_0, 0), (self.low_pass_filter_0, 0))    
+        self.connect((self.blocks_char_to_float_0, 0), (self.blocks_float_to_complex_0, 0))    
+        self.connect((self.blocks_char_to_float_0, 0), (self.blocks_sub_xx_1, 0))    
+        self.connect((self.blocks_char_to_float_0, 0), (self.wxgui_fftsink2_0_0, 0))    
+        self.connect((self.blocks_char_to_float_1, 0), (self.blocks_float_to_complex_1, 1))    
+        self.connect((self.blocks_char_to_float_1, 0), (self.blocks_float_to_complex_1, 0))    
+        self.connect((self.blocks_char_to_float_1, 0), (self.wxgui_scopesink2_0, 1))    
+        self.connect((self.blocks_complex_to_float_0, 0), (self.wxgui_scopesink2_0, 2))    
+        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_multiply_xx_0, 1))    
+        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_sub_xx_0, 1))    
+        self.connect((self.blocks_float_to_complex_1, 0), (self.wxgui_constellationsink2_0, 0))    
+        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_add_xx_0, 0))    
+        self.connect((self.blocks_multiply_xx_0_0, 0), (self.blocks_add_xx_0, 1))    
+        self.connect((self.blocks_repeat_0, 0), (self.blocks_char_to_float_0, 0))    
+        self.connect((self.blocks_sub_xx_0, 0), (self.blocks_multiply_xx_0_0, 1))    
+        self.connect((self.blocks_sub_xx_1, 0), (self.wxgui_scopesink2_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.analog_quadrature_demod_cf_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_complex_to_float_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_constellationsink2_0_0, 0))    
+        self.connect((self.blocks_vector_source_x_0_1, 0), (self.blocks_repeat_0, 0))    
+        self.connect((self.digital_binary_slicer_fb_0, 0), (self.blocks_char_to_float_1, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.blocks_throttle_0, 0))    
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_0_0_0.set_sampling_freq(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 8000, 1000, firdes.WIN_HAMMING, 6.76))
+        self.wxgui_constellationsink2_0.set_sample_rate(self.samp_rate)
+        self.wxgui_constellationsink2_0_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0_0.set_sample_rate(self.samp_rate)
+        self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
+
+
+def main(top_block_cls=top_block_BFSK, options=None):
+
+    tb = top_block_cls()
+    tb.Start(True)
+    tb.Wait()
+
+
+if __name__ == '__main__':
+    main()
