37a7f819c9f148271af9d0ec62aeb6861b4d00c8
lucasebs <lucasebatista@gmail.com>
2018-08-02 07:35:24 -0300

    Ajustes FSK

--- top_block.py
+++ top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Thu Aug  2 00:25:17 2018
+# Generated: Thu Aug  2 07:31:51 2018
 ##################################################
 
 
@@ -21,6 +21,7 @@ from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import digital
 from gnuradio import eng_notation
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
@@ -49,22 +50,18 @@ class top_block(grc_wxgui.top_block_gui):
         ##################################################
         # Blocks
         ##################################################
-        self.notebook_0 = self.notebook_0 = wx.Notebook(self.GetWin(), style=wx.NB_LEFT)
+        self.notebook_0 = self.notebook_0 = wx.Notebook(self.GetWin(), style=wx.NB_TOP)
         self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "fft")
-        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "scope")
-        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "scope2")
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "scopeRx")
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "scopeTx")
         self.Add(self.notebook_0)
-        self.nb1 = self.nb1 = wx.Notebook(self.GetWin(), style=wx.NB_LEFT)
-        self.nb1.AddPage(grc_wxgui.Panel(self.nb1), "fftsource")
-        self.nb1.AddPage(grc_wxgui.Panel(self.nb1), "scopesource")
-        self.Add(self.nb1)
         self.wxgui_scopesink2_0_0 = scopesink2.scope_sink_f(
         	self.notebook_0.GetPage(2).GetWin(),
         	title='Scope Plot',
-        	sample_rate=samp_rate*2,
-        	v_scale=1,
+        	sample_rate=samp_rate,
+        	v_scale=0,
         	v_offset=0,
-        	t_scale=1,
+        	t_scale=0,
         	ac_couple=False,
         	xy_mode=False,
         	num_inputs=1,
@@ -79,36 +76,20 @@ class top_block(grc_wxgui.top_block_gui):
         	v_scale=1,
         	v_offset=0,
         	t_scale=0,
-        	ac_couple=False,
+        	ac_couple=True,
         	xy_mode=False,
         	num_inputs=1,
         	trig_mode=wxgui.TRIG_MODE_AUTO,
         	y_axis_label='Counts',
         )
         self.notebook_0.GetPage(1).Add(self.wxgui_scopesink2_0.win)
-        self.wxgui_fftsink2_0_0 = fftsink2.fft_sink_f(
-        	self.nb1.GetPage(0).GetWin(),
-        	baseband_freq=0,
-        	y_per_div=10,
-        	y_divs=10,
-        	ref_level=0,
-        	ref_scale=2.0,
-        	sample_rate=samp_rate,
-        	fft_size=1024,
-        	fft_rate=15,
-        	average=False,
-        	avg_alpha=None,
-        	title='FFT Plot',
-        	peak_hold=False,
-        )
-        self.nb1.GetPage(0).Add(self.wxgui_fftsink2_0_0.win)
         self.wxgui_fftsink2_0 = fftsink2.fft_sink_f(
         	self.notebook_0.GetPage(0).GetWin(),
         	baseband_freq=0,
         	y_per_div=10,
         	y_divs=10,
-        	ref_level=0,
-        	ref_scale=2.0,
+        	ref_level=30,
+        	ref_scale=4,
         	sample_rate=samp_rate,
         	fft_size=1024,
         	fft_rate=15,
@@ -118,20 +99,22 @@ class top_block(grc_wxgui.top_block_gui):
         	peak_hold=False,
         )
         self.notebook_0.GetPage(0).Add(self.wxgui_fftsink2_0.win)
+        self.low_pass_filter_0 = filter.fir_filter_fff(1, firdes.low_pass(
+        	1, samp_rate, 8000, 1000, firdes.WIN_HANN, 6.76))
         self.digital_binary_slicer_fb_0 = digital.binary_slicer_fb()
-        self.blocks_vector_source_x_0 = blocks.vector_source_b((0, 0, 0,0,0,1,1,0,1,0,1,0,0,1,1,1,0,0,1,0,1), True, 1, [])
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_vector_source_x_0 = blocks.vector_source_b((0,0,0,0,1,0,1,1,0,0,1,0,1,1,1,1,), True, 1, [])
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate/2,True)
         self.blocks_sub_xx_0 = blocks.sub_ff(1)
         self.blocks_repeat_0 = blocks.repeat(gr.sizeof_char*1, 100)
         self.blocks_multiply_xx_0_0 = blocks.multiply_vff(1)
         self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
         self.blocks_float_to_complex_1 = blocks.float_to_complex(1)
         self.blocks_char_to_float_1 = blocks.char_to_float(1, 1)
-        self.blocks_char_to_float_0 = blocks.char_to_float(1, -1)
+        self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
         self.blocks_add_xx_0 = blocks.add_vff(1)
-        self.analog_sig_source_x_0_0_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 4400, 1, 0)
-        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 100, 1, 0)
-        self.analog_quadrature_demod_cf_0 = analog.quadrature_demod_cf(-56)
+        self.analog_sig_source_x_0_0_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 400, 5, 0)
+        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 44000, 5, 0)
+        self.analog_quadrature_demod_cf_0 = analog.quadrature_demod_cf(-100)
         self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 1)
 
         ##################################################
@@ -145,28 +128,28 @@ class top_block(grc_wxgui.top_block_gui):
         self.connect((self.blocks_add_xx_0, 0), (self.blocks_throttle_0, 0))
         self.connect((self.blocks_char_to_float_0, 0), (self.blocks_multiply_xx_0, 1))
         self.connect((self.blocks_char_to_float_0, 0), (self.blocks_sub_xx_0, 0))
-        self.connect((self.blocks_char_to_float_0, 0), (self.wxgui_fftsink2_0_0, 0))
         self.connect((self.blocks_char_to_float_1, 0), (self.wxgui_scopesink2_0, 0))
         self.connect((self.blocks_float_to_complex_1, 0), (self.analog_quadrature_demod_cf_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_add_xx_0, 0))
         self.connect((self.blocks_multiply_xx_0_0, 0), (self.blocks_add_xx_0, 1))
         self.connect((self.blocks_repeat_0, 0), (self.blocks_char_to_float_0, 0))
         self.connect((self.blocks_sub_xx_0, 0), (self.blocks_multiply_xx_0_0, 1))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_float_to_complex_1, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.wxgui_scopesink2_0_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.low_pass_filter_0, 0))
         self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_repeat_0, 0))
         self.connect((self.digital_binary_slicer_fb_0, 0), (self.blocks_char_to_float_1, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.blocks_float_to_complex_1, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.wxgui_scopesink2_0_0, 0))
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.wxgui_scopesink2_0_0.set_sample_rate(self.samp_rate*2)
+        self.wxgui_scopesink2_0_0.set_sample_rate(self.samp_rate)
         self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
-        self.wxgui_fftsink2_0_0.set_sample_rate(self.samp_rate)
         self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 8000, 1000, firdes.WIN_HANN, 6.76))
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate/2)
         self.analog_sig_source_x_0_0_0.set_sampling_freq(self.samp_rate)
         self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
 
